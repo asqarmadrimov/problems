18a5eeef138d8a7f176ff67631fc299e4d9a47ed
asqarmadrimov <madrimov2530@gmail.com>
2022-08-15 23:51:34 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+  // Problem 1 is to speed up words
+
+// let remember = /(\w+)\s(\w+)/;
+// let names = 'Mdrimov Asqar';
+// let resulte = names.replace(re, '$2, $1');
+// console.log(resulte);
+
+
+
+
+  // Problem 2 is to multiply letters
+
+// let names = 'Asqar';
+// let empty = '';
+// for (let i = 0; i < names.length; i++) {
+//   let result = names.charAt(i);
+//   empty += result + result;
+// } 
+// console.log(empty);
+
+
+
+    // Problem 3 order the number
+
+    // let namber = [2, 23,434,32,3,212,213];
+    // let arrResult = [];
+
+    // function prob () {
+    //   for (let i = 0; i < namber.length; i++) {
+    //   if ((Math.sqrt(namber[i]) % 1) == 0) {
+    //       arrResult.push(Math.sqrt(namber[i]));
+    //     } else {
+    //       arrResult.push(Math.pow(namber[i], 2));
+    //     }
+    //     }
+    // }
+    // prob();
+    // console.log(arrResult);
+
